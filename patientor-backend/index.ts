edaf4e22f33b1dbf771a581cdb07356ec9da1898
Igor071 <iburykin071@gmail.com>
2024-01-24 10:00:20 +0200

    9.12: Patientor backend, step5

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
+
 import patientorService from './src/services/patientorService';
+import { Patients } from './src/types/types';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
 
 
 
@@ -25,6 +26,22 @@ app.get('/api/patients', (_req, res) => {
     res.status(200).send(patientorService.getAllPatients());
 });
 
+app.post('/api/patients', (req, res) => {
+    try {
+        const newPatient: Patients = patientorService.addPatient(req.body);
+        res.status(201).send(newPatient);
+    }
+    catch (error) {
+        let errorMessage = 'Something went wrong';
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
+});
+
+
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- patientor-backend/src/services/patientorService.ts
+++ patientor-backend/src/services/patientorService.ts
@@ -1,6 +1,9 @@
 import { Patients, Diagnoses, patientsWithoutSSN } from '../types/types';
-import diagnosesData from '../../data/diagnoses'
+import diagnosesData from '../../data/diagnoses';
 import patientsData from '../../data/patients';
+
+import { v4 as uuid } from 'uuid';
+
 
 
 const diagnoses: Diagnoses[] = diagnosesData;
@@ -16,14 +19,65 @@ const getAllPatients = (): patientsWithoutSSN[] => {
             gender,
             occupation
         };
-    })
+    });
 };
 
 const getAllDiagnoses = (): Diagnoses[] => {
     return diagnoses;
 };
 
+const addPatient = (receivedPatient: unknown): Patients => {
+
+    //Generating random id
+    const id = uuid();
+
+    //Testing for object type (narrowing)
+    if (!receivedPatient || typeof receivedPatient !== 'object') {
+        throw new Error('Incorrect or missing data');
+    }
+
+    //Testing if all required field came (narrowing)
+    if ('name' in receivedPatient &&
+        'dateOfBirth' in receivedPatient &&
+        'ssn' in receivedPatient &&
+        'gender' in receivedPatient &&
+        'occupation' in receivedPatient
+    ) {
+        const newPatient: Patients = {
+            id,
+            name: parseString(receivedPatient.name),
+            dateOfBirth: parseString(receivedPatient.dateOfBirth),
+            ssn: parseString(receivedPatient.ssn),
+            gender: parseString(receivedPatient.gender),
+            occupation: parseString(receivedPatient.occupation)
+        };
+
+        //Saving right object to dummy hardcoded database
+        patients.push(newPatient);
+
+        //Returning saved object for sending back to frontend
+        return newPatient;
+    } else {
+        throw new Error('Incorrect data: some fields are missing!');
+    }
+
+};
+
+//Testing for string type
+const isString = (testObject: unknown): testObject is string => {
+    return typeof testObject === 'string' || testObject instanceof String;
+};
+
+//Parsing incoming field
+const parseString = (testObject: unknown): string => {
+    if (!testObject || !isString(testObject)) {
+        throw new Error('Error in patient description!');
+    }
+
+    return testObject;
+};
+
 
 export default {
-    getAllDiagnoses, getAllPatients
+    getAllDiagnoses, getAllPatients, addPatient
 };
